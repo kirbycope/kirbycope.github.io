48dbb97bad5ff007f5d8ca6b73d59b6aeefe7f53
Timothy Cope <kirbycope@gmail.com>
2023-02-25 17:28:24 -0800

    Update index.js

--- index.js
+++ index.js
@@ -203,9 +203,8 @@ window.onload = function () {
     // Populate the page
     $.getJSON("projects.json", function(data){
         populateProjects(data);
+        // https://www.w3schools.com/howto/howto_js_portfolio_filter.asp
+        filterSelection("all");
     });
-
-    // https://www.w3schools.com/howto/howto_js_portfolio_filter.asp
-    filterSelection("all");
 
 };
