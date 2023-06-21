c4c5db23823114f72395ac7e163a9eefd6f5b609
Timothy Cope <kirbycope@gmail.com>
2023-06-20 18:12:28 -0700

    Groovy

--- index.js
+++ index.js
@@ -62,6 +62,9 @@ function populateProjects(projects) {
         else if (projects[i].small.indexOf("Creation Kit") !== -1) {
             cardImageOverlay.setAttribute("src", "/img/creation_kit_white.png");
         }
+        else if (projects[i].small.indexOf("Groovy") !== -1) {
+            cardImageOverlay.setAttribute("src", "/img/groovy.png");
+        }
         else if (projects[i].small.indexOf("Java") !== -1 && projects[i].small.indexOf("JavaScript") == -1) {
             cardImageOverlay.setAttribute("src", "/img/java.png");
         }
